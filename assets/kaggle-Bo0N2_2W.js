bc878ab6e0752b392ce4f72c7f4bc151a59cf86c
Senash Thenuja <senathenu@gmail.com>
2024-11-27 02:45:45 +1300

    Updates

---
+++ assets/kaggle-Bo0N2_2W.js
@@ -0,0 +1,5 @@
+import{r as e}from"./index-C4TiTpA_.js";const l=c=>e.createElement("svg",{id:"Layer_1","data-name":"Layer 1",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",...c},e.createElement("defs",null,e.createElement("style",null,`
+      .cls-1 {
+        stroke-width: 0px;
+      }
+    `)),e.createElement("path",{className:"cls-1",d:"M19,2H5c-1.7,0-3,1.3-3,3v14c0,1.7,1.3,3,3,3h14c1.7,0,3-1.3,3-3V5c0-1.7-1.3-3-3-3ZM16,19c0,0,0,0-.2,0h-1.8c-.1,0-.2,0-.3-.1l-3-3.9-.8.8v2.7c0,.3-.2.5-.5.5h-.9c-.3,0-.5-.2-.5-.5V5.5c0-.3.2-.5.5-.5h.9c.3,0,.5.2.5.5v7.4c0,.3.3.4.5.2l3.2-3.2c0,0,.2-.1.3-.1h1.9c0,0,.1,0,.2,0,0,0,0,.2,0,.2l-3.7,3.5c-.1.1-.1.3,0,.4l3.8,4.8c0,0,0,.1,0,.2Z"}));export{l as default};
