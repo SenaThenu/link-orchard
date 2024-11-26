5acf9c20b29d0560defe7c2a8a6f2c152832054d
Senash Thenuja <senathenu@gmail.com>
2024-11-26 20:45:56 +1300

    💄 style: link block animation

--- src/components/LinkBlock.tsx
+++ src/components/LinkBlock.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 // components
 import Icon from "./Icon";
 
@@ -11,17 +13,36 @@ interface LinkBlockProps {
     logoColor: string;
 }
 
+const linkBlockVariants = {
+    initial: { scale: 1 },
+    hover: { scale: 1.05 },
+    focus: { scale: 1.05 },
+    tap: { scale: 0.9 },
+};
+
 export default function LinkBlock(props: LinkBlockProps) {
     return (
         <a className="link-block" href={props.url} target="_blank">
-            <div className="link-content">
+            <motion.div
+                className="link-content"
+                variants={linkBlockVariants}
+                initial="initial"
+                whileHover="hover"
+                whileFocus="focus"
+                whileTap="tap"
+                transition={{
+                    duration: 0.1,
+                    type: "spring",
+                    damping: 25,
+                    stiffness: 500,
+                }}>
                 <Icon
                     fillColor={props.logoColor}
                     name={props.logoName}
                     size={42}
                 />
                 <div className="link-name">{props.linkName}</div>
-            </div>
+            </motion.div>
         </a>
     );
 }
