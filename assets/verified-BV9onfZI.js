f7ce55edec7913cf6cc15be66796955ac76f1928
Senash Thenuja <senathenu@gmail.com>
2024-11-27 16:42:11 +1300

    Updates

--- assets/verified-BV9onfZI.js
+++
@@ -1,5 +0,0 @@
-import{r as e}from"./index-C51KNVwg.js";const c=s=>e.createElement("svg",{id:"Layer_1","data-name":"Layer 1",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",...s},e.createElement("defs",null,e.createElement("style",null,`
-      .cls-1 {
-        stroke-width: 0px;
-      }
-    `)),e.createElement("path",{className:"cls-1",d:"M22,12c0-1.4-.8-2.7-2.5-3.1,1.8-2.9-1.5-6.2-4.4-4.4-.4-1.7-1.7-2.5-3.1-2.5s-2.7.8-3.1,2.5c-2.3-1.4-4.9.4-4.9,2.7s.2,1.2.5,1.7c-1.7.4-2.5,1.7-2.5,3.1s.8,2.7,2.5,3.1c-1.5,2.3.4,4.9,2.7,4.9s1.2-.2,1.7-.5c.4,1.7,1.7,2.5,3.1,2.5s2.7-.8,3.1-2.5c2.3,1.5,4.9-.4,4.9-2.7s-.2-1.1-.5-1.7c1.7-.4,2.5-1.8,2.5-3.1ZM15.9,10.6l-4.2,4.2c-.4.4-1.1.4-1.6,0l-2-2.1c-.4-.4-.4-1,0-1.4s1-.4,1.4,0l1.4,1.4,3.5-3.5c.4-.4,1-.4,1.4,0s.4,1,0,1.4Z"}));export{c as default};
