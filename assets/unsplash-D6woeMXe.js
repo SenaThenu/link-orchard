526c47f91aa328820151b73d33f716228487ea68
Senash Thenuja <senathenu@gmail.com>
2024-11-27 15:53:28 +1300

    Updates

---
+++ assets/unsplash-D6woeMXe.js
@@ -0,0 +1,5 @@
+import{r as e}from"./index-C51KNVwg.js";const a=c=>e.createElement("svg",{id:"Layer_1","data-name":"Layer 1",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",...c},e.createElement("defs",null,e.createElement("style",null,`
+      .cls-1 {
+        stroke-width: 0px;
+      }
+    `)),e.createElement("path",{className:"cls-1",d:"M15.9,15.4v-3.3c0-.6.5-1.1,1.1-1.1h3.9c.6,0,1.1.5,1.1,1.1v8.9c0,.6-.5,1.1-1.1,1.1H3.1c-.6,0-1.1-.5-1.1-1.1v-8.9c0-.6.5-1.1,1.1-1.1h3.9c.6,0,1.1.5,1.1,1.1v3.3c0,.6.5,1.1,1.1,1.1h5.7c.6,0,1.1-.5,1.1-1.1ZM9.2,2h5.5c.6,0,1.1.5,1.1,1.1v3.4c0,.6-.5,1.1-1.1,1.1h-5.5c-.6,0-1.1-.5-1.1-1.1v-3.4c0-.6.5-1.1,1.1-1.1Z"}));export{a as default};
